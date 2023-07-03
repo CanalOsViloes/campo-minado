c226f4824dc6c8decc820febb5e81f3b6166cbcb
CanalOsViloes <100821773+CanalOsViloes@users.noreply.github.com>
2023-07-03 09:15:09 -0300

    Update sketch.js
    Final do vídeo parte 2

--- sketch.js
+++ sketch.js
@@ -14,13 +14,43 @@ function criaArray2D(colunas, linhas){
 }
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(401, 401);
   
   grid = criaArray2D(col, lin);
   
   for(let i=0;i<lin;i++){
     for(let j=0;j<col;j++){
-      grid[i][j] = new Cell(i, j,lado,Math.random() < 0.5);
+      grid[i][j] = new Cell(i, j,lado,Math.random() < 0.1);
+    }
+  }
+  
+  for(let i=0;i<lin;i++){
+    for(let j=0;j<col;j++){
+      
+      let bombasAoRedor = 0;
+      
+      for(let x=-1;x<2;x++){
+        for(let y=-1;y<2;y++){
+          
+          if(x==0 && y==0){
+            continue;
+          }
+          
+          let linhaCheck = i+y;
+          let colunaCheck = j+x;
+          
+          if(linhaCheck<0 || colunaCheck<0 || linhaCheck>=lin || colunaCheck>=col){
+            continue;
+          }
+          
+          if(grid[linhaCheck][colunaCheck].temBomba()){
+            bombasAoRedor++;
+          }
+          
+        }
+      }
+      
+      grid[i][j].setQuantidadeBombasAoRedor(bombasAoRedor);
     }
   }
   
@@ -37,6 +67,40 @@ function draw() {
 
 function abrirCelula(i,j){
   grid[i][j].abrir();
+  if(grid[i][j].bombasAoRedor==0){
+    floodFill(i,j);
+  }
+}
+
+function floodFill(iOrigem, jOrigem){
+  grid[iOrigem][jOrigem].setPesquisado();
+  
+  for(let x=-1;x<2;x++){
+    for(let y=-1;y<2;y++){
+      
+      if(x==0 && y==0){
+        continue;
+      }
+      
+      let linhaCheck = iOrigem+y;
+      let colunaCheck = jOrigem+x;
+      
+      if(linhaCheck<0 || colunaCheck<0 || linhaCheck>=lin || colunaCheck>=col){
+        continue;
+      }   
+      
+      if(!grid[linhaCheck][colunaCheck].pesquisado){
+        grid[linhaCheck][colunaCheck].setPesquisado();
+        if(!grid[linhaCheck][colunaCheck].temBomba()){
+           abrirCelula(linhaCheck,colunaCheck);
+        }
+      }
+      
+      
+    }
+  }
+  
+  
 }
 
 function mouseClicked(){
