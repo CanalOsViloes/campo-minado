aa928da5bdc6a216307595d30080abf4a43553ef
CanalOsViloes <100821773+CanalOsViloes@users.noreply.github.com>
2023-07-06 20:15:27 -0300

    Versão FINAL

--- sketch.js
+++ sketch.js
@@ -2,6 +2,11 @@ let grid = [];
 let lin = 10;
 let col = 10;
 let lado = 40;
+let qtdeBombas = 10;
+let fimDeJogo = false;
+let celulasFechadas = 0;
+let vitoria = false;
+let tempo = 0;
 
 function criaArray2D(colunas, linhas){
   let arr = new Array(linhas);
@@ -20,7 +25,20 @@ function setup() {
   
   for(let i=0;i<lin;i++){
     for(let j=0;j<col;j++){
-      grid[i][j] = new Cell(i, j,lado,Math.random() < 0.1);
+      grid[i][j] = new Cell(i, j,lado,false);
+      celulasFechadas++;
+    }
+  }
+  
+  let bombasTemp = qtdeBombas;
+  
+  while(bombasTemp>0){
+    let randomI = floor(random(0,col-1));
+    let randomJ = floor(random(0,lin-1));
+    
+    if(!grid[randomI][randomJ].temBomba()){
+      grid[randomI][randomJ].setBomba();
+      bombasTemp--;
     }
   }
   
@@ -63,13 +81,62 @@ function draw() {
       grid[i][j].desenhar();
     }
   }
+  
+  if(frameCount%60==0 && !fimDeJogo){
+    tempo++;
+  }
+  
+  if(fimDeJogo){
+    if(vitoria){
+      push();
+        fill('black');
+        textStyle(BOLD);
+        textSize(30);
+        textAlign(CENTER,CENTER);
+        text("VITÓRIA!",200,130);
+        text("Tempo:"+tempo+"s",200,200);
+      pop();
+    }else{
+      push();
+        fill('black');
+        textStyle(BOLD);
+        textSize(30);
+        textAlign(CENTER,CENTER);
+        text("DERROTA!",200,200);
+      pop();
+    }
+  }
+}
+
+function gameOver(){
+  fimDeJogo = true;
+  abreCelulasBomba();
+}
+
+function abreCelulasBomba(){
+  for(let i=0;i<lin;i++){
+      for(let j=0;j<col;j++){
+        if(grid[i][j].temBomba()){
+          grid[i][j].abrir();
+        }
+      }
+  }
 }
 
 function abrirCelula(i,j){
-  grid[i][j].abrir();
-  if(grid[i][j].bombasAoRedor==0){
-    floodFill(i,j);
+  if(!grid[i][j].marcado && !grid[i][j].aberto){
+    grid[i][j].abrir();  
+    celulasFechadas--;
+    console.log(celulasFechadas);
+    if(grid[i][j].temBomba()){
+      gameOver();
+    }
+    if(grid[i][j].bombasAoRedor==0){
+      floodFill(i,j);
+    }
   }
+  
+  
 }
 
 function floodFill(iOrigem, jOrigem){
@@ -103,10 +170,32 @@ function floodFill(iOrigem, jOrigem){
   
 }
 
+function checarVitoria(){
+  if(celulasFechadas<=qtdeBombas){
+    fimDeJogo = true;
+    vitoria = true;
+    abreCelulasBomba();
+  }
+}
+
+function marcarCelula(i,j){
+  grid[i][j].marcarBomba();
+}
+
 function mouseClicked(){
+
   let j = floor(mouseX/lado);
   let i = floor(mouseY/lado);
   
-  abrirCelula(i,j);
+  if(!fimDeJogo){
+    if(keyIsDown(18)){
+      marcarCelula(i,j);
+    }else{
+      abrirCelula(i,j);  
+    }
   
+    checarVitoria();  
+  }
+  
+ 
 }
