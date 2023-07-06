83d2394fe79c2236c8b14b1b521df424d91e76e3
CanalOsViloes <100821773+CanalOsViloes@users.noreply.github.com>
2023-07-06 20:14:58 -0300

    Versão FINAL

--- cell.js
+++ cell.js
@@ -7,22 +7,43 @@ class Cell{
     this.aberto = false;
     this.bombasAoRedor = 0;
     this.pesquisado = false;
+    this.marcado = false;
   }
-  
+ 
   desenhar(){
     if(this.aberto){
       if(this.tenhoBomba){
         fill(color("red"));  
         rect(this.j*this.lado, this.i*this.lado, this.lado);
       }else{
-        fill(color("green"));  
+        fill("#CCFFCC"); 
         rect(this.j*this.lado, this.i*this.lado, this.lado);
         if(this.bombasAoRedor!=0){
+          let cor = color(0,0,0);
+          switch(this.bombasAoRedor){
+              case 1:
+                cor = color(0,128,255);
+                break;
+              case 2:
+                cor = color(0,200,0);
+                break;
+              case 3:
+                cor = color(255,0,0);
+                break;
+              case 4:
+                cor = color(127,0,255);
+                break;
+              case 5:
+                cor = color(255,0,255);
+                break;
+              default:
+            }
           push();
-            fill(color('black'));
-            strokeWeight(5);
+            fill(cor);
+            strokeWeight(7);
             textAlign(CENTER,CENTER);
-            textSize(15);
+            textSize(18);
+          textStyle(BOLD);
             text(this.bombasAoRedor,
                  this.j*this.lado+this.lado/2,
                  this.i*this.lado+this.lado/2,
@@ -30,12 +51,18 @@ class Cell{
           pop();
         }
       }
-      
+     
     }else{
-      fill(color("grey"));
-      rect(this.j*this.lado, this.i*this.lado, this.lado);
+      if(this.marcado){
+         fill(color("pink"));
+        rect(this.j*this.lado, this.i*this.lado, this.lado);  
+      }else{
+        fill(color("grey"));
+        rect(this.j*this.lado, this.i*this.lado, this.lado);  
+      }
+     
     }
-    
+   
     if(this.aberto && this.tenhoBomba){
       push();
         fill(color('black'));
@@ -45,22 +72,30 @@ class Cell{
                this.lado/4)
       pop();
     }
-    
+   
   }
-  
+ 
   setQuantidadeBombasAoRedor(num){
     this.bombasAoRedor = num;
   }
-  
+ 
   temBomba(){
     return this.tenhoBomba;
   }
-  
+ 
   abrir(){
     this.aberto = true;
   }
-  
+ 
+  setBomba(){
+    this.tenhoBomba = true;
+  }
+ 
   setPesquisado(){
     this.pesquisado = true;
+  }
+ 
+  marcarBomba(){
+    this.marcado = !this.marcado;
   }
 }
